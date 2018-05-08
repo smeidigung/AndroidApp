0d892c96b76ed66765d49e8c6a6a8ca10aa11103
esmede14 <esmede14@student.aau.dk>
2018-05-08 13:37:25 +0200

    New application

---
+++ SleepAndroidApplication/app/src/main/java/main/sleepapp/MainController.java
@@ -0,0 +1,13 @@
+package main.sleepapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainview);
+    }
+}
